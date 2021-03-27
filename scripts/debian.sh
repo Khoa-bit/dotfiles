ddea1f21e10b48d4e59e740ffb5c77e970ef14ed
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2021-03-27 22:58:45 +0700

    Add Install Scripts for Arch and Debian

---
+++ scripts/debian.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env sh
+
+set -o errexit -o nounset
+
+apt_ins="apt install -y"
+ppa_ins="add-apt-repository"
+snap_ins="snap install -y"
+
+apt_install() {
+    # ttf-google-fonts-git, ttf-mac-fonts are not installed.
+    # scrcpy for android app development :>
+    # minecraft-server for :>
+
+  sudo $apt_ins zsh \
+    net-tools dconf-editor gnome-tweaks chrome-gnome-shell
+    calibre qbittorrent discord \
+    ffmpeg youtube-dl vlc \
+    git unrar gitkraken \
+    code codium
+}
+
+ppa_install() {
+  sudo $ppa_ins ppa:musicbrainz-developers/stable
+  sudo $ppa_ins ppa:agornostal/ulauncher
+  sudo $ppa_ins ppa:bamboo-engine/ibus-bamboo
+
+  sudo $apt_ins picard ulauncher ibus-bamboo
+
+  ibus restart
+  gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
+}
+
+snap_install() {
+  pkgs = "chromium"
+  classic_pkgs = "intellij-idea-ultimate pycharm-professional"
+
+  sudo $snap_ins $pkgs
+  sudo $snap_ins --classic $classic_pkgs
+
+  cp /var/lib/snapd/desktop/applications/* ~/.local/share/applications/
+
+}
+
+install_ms_font() {
+    sudo apt update && sudo apt install ttf-mscorefonts-installer
+}
+
+apt_install
+
+snap_install
+
+install_ms_font
+
+echo "=========================================="
+echo "Copied snap apps into local's applications folder for applying icon pack"
+echo "Cuz I don't know how to edit files automaticly, Please edit ICON tag of .desktop files :>"
+echo "=========================================="
+echo "XDMan apps must be manually install, unfortunately. :<"
+echo "https://github.com/subhra74/xdm/releases"
+echo "=========================================="
